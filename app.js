944272a2cd29283ebe176be4f1b1b244fe7184ff
destin floyd <destin.b.floyd@gmail.com>
2018-12-16 22:58:52 -0500

    added comments checked formatiing

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 // console.log("working")
 // $('body').css('background', 'pink')
 
-
-// NOTESSSSSSSSSS
-// b25.val() = 0.1;
-// b25.css("background", p1Color) 
-
+// all boxes on board
 let b11 = $('.column1 .row1')
 let b12 = $('.column1 .row2')
 let b13 = $('.column1 .row3')
@@ -55,104 +51,26 @@ let b74 = $('.column7 .row4')
 let b75 = $('.column7 .row5')
 let b76 = $('.column7 .row6')
 
-let color = 'green';
+// colors for players
+let color = 'white';
 let p1Color = 'red';
 let p2Color = 'black';
 
+// default score box and p1/p2 values for tracking which squares are checked
 let score = 0;
-let p1Score = .1;
-let p2Score = 1;
+const p1Score = .1;
+const p2Score = 1;
 
 let turnsCounter = 0;
 
+// score board tracker vars
 let numOfWins1 = 0
 let numOfWins2 = 0
 
+// array holding mini 4 spot arrays of all winning positions.
 const wins = [[b11, b21, b31, b41], [b12, b22, b32, b42], [b13, b23, b33, b43], [b14, b24, b34, b44], [b15, b25, b35, b45], [b16, b26, b36, b46], [b21, b31, b41, b51], [b22, b32, b42, b52], [b23, b33, b43, b53], [b24, b34, b44, b54], [b25, b35, b45, b55], [b26, b36, b46, b56], [b31, b41, b51, b61], [b32, b42, b52, b62], [b33, b43, b53, b63], [b34, b44, b54, b64], [b35, b45, b55, b65], [b36, b46, b56, b66], [b41, b51, b61, b71], [b42, b52, b62, b72], [b43, b53, b63, b73], [b44, b54, b64, b74], [b45, b55, b65, b75], [b46, b56, b66, b76], [b11, b12, b13, b14], [b21, b22, b23, b24], [b31, b32, b33, b34], [b41, b42, b43, b44], [b51, b52, b53, b54], [b61, b62, b63, b64], [b71, b72, b73, b74], [b12, b13, b14, b15], [b22, b23, b24, b25], [b32, b33, b34, b35], [b42, b43, b44, b45], [b52, b53, b54, b55], [b62, b63, b64, b65], [b72, b73, b74, b75], [b13, b14, b15, b16], [b23, b24, b25, b26], [b33, b34, b35, b36], [b43, b44, b45, b46], [b53, b54, b55, b56], [b63, b64, b65, b66], [b73, b74, b75, b76], [b16, b25, b34, b43], [b26, b35, b44, b53], [b36, b45, b54, b63], [b46, b55, b64, b73], [b46, b35, b24, b13], [b56, b45, b34, b23], [b66, b55, b44, b33], [b76, b65, b54, b43], [b15, b24, b33, b42], [b25, b34, b43, b52], [b35, b44, b53, b62], [b45, b54, b63, b72], [b45, b34, b23, b12], [b55, b44, b33, b22], [b65, b54, b43, b32], [b75, b64, b53, b42], [b14, b23, b32, b41], [b24, b33, b42, b51], [b34, b43, b52, b61], [b44, b53, b62, b71], [b44, b33, b22, b11], [b54, b43, b32, b21], [b64, b53, b42, b31], [b74, b63, b52, b41]]
 
-
-// [b11, b21, b31, b41],
-//  [b12, b22, b32, b42],
-//   [b13, b23, b33, b43],
-//    [b14, b24, b34, b44], 
-//    [b15, b25, b35, b45], 
-//    [b16, b26, b36, b46], 
-
-//    [b21, b31, b41, b51], 
-//    [b22, b32, b42, b52], 
-//    [b23, b33, b43, b53] , 
-//    [b24, b34, b44, b54], 
-//    [b25, b35, b45, b55], 
-//    [b26, b36, b46, b56], 
-
-//    [b31, b41, b51, b61], 
-//    [b32, b42, b52, b62], 
-//    [b33, b43, b53, b63], 
-//    [b34, b44, b54, b64], 
-//    [b35, b45, b55, b65], 
-//    [b36, b46, b56, b66], 
-
-//    [b41, b51, b61, b71], 
-//    [b42, b52, b62, b72], 
-//    [b43, b53, b63, b73],
-//     [b44, b54, b64, b74],
-//      [b45, b55, b65, b75],
-//       [b46, b56, b66, b76],
-
-//        [b11, b12, b13, b14], 
-//        [b21, b22, b23, b24], 
-//        [b31, b32, b33, b34], 
-//        [b41, b42, b43, b44], 
-//        [b51, b52, b53, b54], 
-//        [b61, b62, b63, b64], 
-//        [b71, b72, b73, b74], 
-
-//        [b12, b13, b14, b15],
-//         [b22, b23, b24, b25],
-//          [b32, b33, b34, b35],
-//           [b42, b43, b44, b45],
-//            [b52, b53, b54, b55], 
-//            [b62, b63, b64, b65], 
-//            [b72, b73, b74, b75], 
-
-//            [b13, b14, b15, b16],
-//             [b23, b24, b25, b26], 
-//             [b33, b34, b35, b36],
-//              [b43, b44, b45, b46],
-//               [b53, b54, b55, b56], 
-//               [b63, b64, b65, b66], 
-//               [b73, b74, b75, b76], 
-
-//               [b16, b25, b34, b43], 
-//               [b26, b35, b44, b53], 
-//               [b36, b45, b54, b63], 
-//               [b46, b55, b64, b73], 
-//               [b46, b35, b24, b13], 
-//               [b56, b45, b34, b23], 
-//               [b66, b55, b44, b33], [
-//                b76, b65, b54, b43],
-
-//                    [b15, b24, b33, b42],
-//                     [b25, b34, b43, b52], 
-//                     [b35, b44, b53, b62], 
-//                     [b45, b54, b63, b72], 
-//                     [b45, b34, b23, b12],
-//                     [b55, b44, b33, b22], 
-//                     [b65, b54, b43, b32], 
-//                     [b75, b64, b53, b42], 
-
-//                     [b14, b23, b32, b41], 
-//                     [b24, b33, b42, b51] 
-//                     [b34, b43, b52, b61], 
-//                     [b44, b53, b62, b71],
-//                      [b44, b33, b22, b11], 
-//                      [b54, b43, b32, b21], [
-//                          b64, b53, b42, b31],
-//                           [b74, b63, b52, b41]]
-
-
-
-let checkTurns = () => {
+const checkTurns = () => {
     turnsCounter++;
     // even p1
     if (turnsCounter % 2 == 0) {
@@ -170,7 +88,7 @@ let checkTurns = () => {
 // console.log(score)
 // console.log(color)
 
-let switchSpots = (sp1, sp2, sp3, sp4, sp5, sp6) => {
+const switchSpots = (sp1, sp2, sp3, sp4, sp5, sp6) => {
 
     if ($(sp6).css('background-color') == 'rgb(255, 255, 255)') {
         $(sp6).attr("value", score)
@@ -201,7 +119,7 @@ let switchSpots = (sp1, sp2, sp3, sp4, sp5, sp6) => {
 
 
 
-let checkForWins = () => {
+const checkForWins = () => {
     for (let counter = 0; counter < wins.length; counter++) {
         let piece = wins[counter]
 
@@ -212,10 +130,7 @@ let checkForWins = () => {
             turnsCounter = 0
             numOfWins1 ++
             $(".p1score").text(numOfWins1)
-            // $(b11).css('background-color', 'white') 
-            // all backgrounds white, 
-            // score board updated
-            // reset all values to 0
+
         }
         else if (piecesValue == 4) {
             console.log("player2 wins!")
@@ -223,14 +138,13 @@ let checkForWins = () => {
             turnsCounter = 0
             numOfWins2 ++
             $(".p2score").text(numOfWins2)
-            // dittOOOO
 
         }
 
     }
 }
 
-let boxes = [b11, b12, b13, b14, b15, b16, b21, b22, b23, b24, b25, b26, b31, b32, b33, b34, b35, b36, b41, b42, b43, b44, b45, b46, b51, b52, b53, b54, b55, b56, b61, b62, b63, b64, b65, b66, b71, b72, b73, b74, b75, b76]
+const boxes = [b11, b12, b13, b14, b15, b16, b21, b22, b23, b24, b25, b26, b31, b32, b33, b34, b35, b36, b41, b42, b43, b44, b45, b46, b51, b52, b53, b54, b55, b56, b61, b62, b63, b64, b65, b66, b71, b72, b73, b74, b75, b76]
 
 const reset = () => {
     for(let i =0; i < boxes.length; i ++){
@@ -238,57 +152,3 @@ const reset = () => {
         $(boxes[i]).attr('value', 0)
     }
 }
-// console.log(parseInt($(b11).attr("value")) +  parseInt($(b12).attr("value")))
-
-
-// if( ){
-
-// }
-
-
-
-
-
-
-// b25.css("background", p1Color) 
-// var back = $(box).css( "background-color" );
-// console.log(back)
-// }
-// switchSpot(b25)
-// switchSpot(b24)
-
-// let switchSpots = (a) => {
-//     let check6 = `".` + `${a}` + `.row1"`
-
-//     console.log(check6)
-//     console.log($("\"." `${check6}"`))
-
-//    if( ){
-//        console.log("okay")
-//    }else if($(`.${a} .row1`).text() == "1" ){
-//        console.log('one')
-//    }
-//    else{
-//        console.log("try agian")
-//    }
-
-    // console.log('clicked')
-    // console.log($(`.${a} .row2`).text())
-    // console.log(parseInt($(`.${a} .row2`).val()))
-// }
-// console.log(b11.val())
-
-
-
-// console.log(b11.attr('value'))
-// var column1 = "column1"
-
-
-
-// switchSpots(column1)
-// if(b13.text() === b11.text()){
-//     console.log('even')
-// } else{
-//     console.log("not")
-// }
-
